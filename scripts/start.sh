a22e3b46517e585841bd85c7630acfeaab8b2a12
johann.d <johann.d@springcard.com>
2026-05-12 18:38:21 +0200

    Improved scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,6 +5,6 @@ sleep 1
 service pcscd start
 pkill -f "mobco-calypso-pki.py" || true
 sleep 1
-python ../calypso-pki/mobco-calypso-pki.py -r "*Puck*00 00" -f ./authorized_cards.txt -o ./open-1.sh
-python ../calypso-pki/mobco-calypso-pki.py -r "*SpringPark*00 00" -f ./authorized_cards.txt -o ./open-2.sh
-python ../calypso-pki/mobco-calypso-pki.py -r "*Architect*00 00" -f ./authorized_cards.txt -o ./open-3.sh
+python ../calypso-pki/mobco-calypso-pki.py -r "*Puck*00 00" -f ./authorized_cards.txt -o ./open-1.sh &
+python ../calypso-pki/mobco-calypso-pki.py -r "*SpringPark*00 00" -f ./authorized_cards.txt -o ./open-2.sh &
+python ../calypso-pki/mobco-calypso-pki.py -r "*Architect*00 00" -f ./authorized_cards.txt -o ./open-3.sh &
